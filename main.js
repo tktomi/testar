bfc5cc77a0a00aa69eae8a1f1fdf5b50f635d1a7
tktomi <taketomi.tak@gmail.com>
2023-03-03 18:57:55 +0900

    main.jsでimport

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import {loadGLTF} from "/libs/loader.js";
+import {loadGLTF} from "./libs/loader.js";
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
